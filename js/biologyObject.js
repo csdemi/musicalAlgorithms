d3123e24f2035a5726c0e6d4e88533673aa44aac
csdemi <cdemianew@gmail.com>
2016-01-21 10:46:21 -0800

    Update biologyObject.js

--- js/biologyObject.js
+++ js/biologyObject.js
@@ -1,6 +1,8 @@
-function biologyObject(id)
+function biologyObject()
 {
-    this.VoiceID = id;//Integer
-    this.letters = ["W","F","L","I","M","Y","V","C","P","T","A","S","Q","N","G","H","R","E","D","K"];//Character
-    this.letterValues=[-2.1,-1.7,-1.3,-1.1,-0.7,-0.7,-0.5,0.0,0.1,0.3,0.5,0.5,0.8,0.9,1.2,1.2,1.8,1.9,2.0,2.8];//Double
+   //this.letters = ["W","F","L","I","M","Y","V","C","P","T","A","S","Q","N","G","H","R","E","D","K"];//Character
+   // this.letterValues=[-2.1,-1.7,-1.3,-1.1,-0.7,-0.7,-0.5,0.0,0.1,0.3,0.5,0.5,0.8,0.9,1.2,1.2,1.8,1.9,2.0,2.8];//Double
+    this.dnaValues=[0,1,2,3];//Values A=0,T=1,C=2,G=3
+    this.originalDNASequence=["A","C","A","T","G","A","G","A","C","A","G","A","C","A","G","A","C","C","C","C","C","A","G","A","G","A","C","A","G","A","C","C","C","C","T","A","G","A","C","A","C","A","G","A","G","A","G","A","G","T","A","T","G","C","A","G","G","A","C","A","G","G","G"];
+    this.userSequenceArray=new Array();
 }
