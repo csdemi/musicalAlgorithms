56084103bf1d3e43be0ec60484a537781135e4f4
RisouKenshi <samplenhold@hotmail.com>
2016-01-29 16:42:32 -0800

    Formatted code, added some comments

--- js/pitchInput.js
+++ js/pitchInput.js
@@ -24,36 +24,46 @@ $(document).ready(function()
             var $dnaLabel = $panel.find('[id^=dna]');//Finds the DNA label so we can display the Sequence label
             var $sequenceInput =$panel.find('[id^=sequence]');//Finds the type of sequence that is going on
             var $textAreaInput=$panel.find('[id^=areaPitch]');//Finds the ID for the AreaText to clear it when DNA/RNA/Proteins ect are choosen
+            
             var $letterA=$panel.find('[id^=A]');//Label for the letter A so we can show and hide it as needed
             var $letterT=$panel.find('[id^=T]');
             var $letterU=$panel.find('[id^=U]');
             var $letterG=$panel.find('[id^=G]');
             var $letterC=$panel.find('[id^=C]');
+            
             var $letterAText=$panel.find('[id^=letterAText]');//The text area for the letter A so we can change values if the user wants to.
             var $letterTText=$panel.find('[id^=letterTText]');
             var $letterUText=$panel.find('[id^=letterUText]');
             var $letterCText=$panel.find('[id^=letterCText]');
             var $letterGText=$panel.find('[id^=letterGText]');
+            
             var $buttonConvert=$panel.find('[id^=convert]');
             var $buttonDuplicate=$panel.find('[id^=duplicates]');
+            
             if($SelectedAlgorithm.text()=="DNA"||$SelectedAlgorithm.text()=="RNA")
             { 
                 $dnaLabel.show();
+                
                 $sequenceInput.show();
                 $textAreaInput.val("");
                 $NoteCount.val("");
+                
                 $letterA.show();
                 $letterC.show();
                 $letterG.show();
+                
                 $letterAText.show();
                 $letterCText.show();
                 $letterGText.show();
+                
                 $buttonConvert.show();
                 $buttonDuplicate.show();
+                
                 if($SelectedAlgorithm.text()=="RNA")
                 {
                     $letterT.hide();
                     $letterU.show();
+                    
                     $letterTText.hide();
                     $letterUText.show();
                 }
@@ -61,43 +71,53 @@ $(document).ready(function()
                 {
                     $letterT.show();
                     $letterU.hide();
+                    
                     $letterTText.show();
                     $letterUText.hide();
+                    
                     $sequenceInput.val(voiceArray[voiceNumber-1].biology.originalDNASequence);
+                    
                     voiceArray[voiceNumber-1].biology.userSequenceArray=$sequenceInput.val().split(",");
+                    
                     $NoteCount.val(voiceArray[voiceNumber-1].biology.userSequenceArray.length);
+                    
                     $letterAText.val(voiceArray[voiceNumber-1].biology.dnaValues[0]);
                     $letterTText.val(voiceArray[voiceNumber-1].biology.dnaValues[1]);
                     $letterCText.val(voiceArray[voiceNumber-1].biology.dnaValues[2]);
                     $letterGText.val(voiceArray[voiceNumber-1].biology.dnaValues[3]);
+                    
                     var i=0;
+                    
                     for(i;i<voiceArray[voiceNumber-1].biology.userSequenceArray.length;i++)
+                    {
+                        if(voiceArray[voiceNumber-1].biology.userSequenceArray[i]=="A")
                         {
-                            if(voiceArray[voiceNumber-1].biology.userSequenceArray[i]=="A")
-                                {
-                                    voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[0];
-                                }
-                            else if(voiceArray[voiceNumber-1].biology.userSequenceArray[i]=="T")
-                                {
-                                    voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[1];
-                                }
-                            else if(voiceArray[voiceNumber-1].biology.userSequenceArray[i]=="C")
-                                {
-                                    voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[2];
-                                }
-                            else
-                                {
-                                    voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[3];
-                                }
+                                voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[0];
                         }
+                        else if(voiceArray[voiceNumber-1].biology.userSequenceArray[i]=="T")
+                        {
+                                voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[1];
+                        }
+                        else if(voiceArray[voiceNumber-1].biology.userSequenceArray[i]=="C")
+                        {
+                                voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[2];
+                        }
+                        else
+                        {
+                                voiceArray[voiceNumber-1].biology.userSequenceArray[i]=voiceArray[voiceNumber-1].biology.dnaValues[3];
+                        }
+                    }
+                    
                     $TextBox.val(voiceArray[voiceNumber-1].biology.userSequenceArray);
                     
                     voiceArray[voiceNumber-1].originalPitchArray=voiceArray[voiceNumber-1].biology.userSequenceArray;
+                    
                     UpdatePitchMappingArray(voiceArray[voiceNumber - 1], GetCurrentSelectedPitchMappingAlgorithm(voiceNumber), voiceArray[voiceNumber - 1].pitchMappingArrayLowerBound, voiceArray[voiceNumber - 1].pitchMappingArrayUpperBound);
                     UpdateFinalPitchArray(voiceArray[voiceNumber - 1], GetCurrentSelectedScale(voiceNumber), voiceArray[voiceNumber - 1].pitchMappingArrayLowerBound, voiceArray[voiceNumber - 1].pitchMappingArrayUpperBound); 
                     UpdateDurationNoteCount(voiceArray[voiceNumber - 1],voiceNumber);
                     UpdateDurationMappingNoteCount(voiceArray[voiceNumber - 1]);
                     UpdateDurationInputTextBoxFromPitchInput(voiceArray[voiceNumber - 1], voiceNumber);
+                    
                     LoadDurationMappingInputTextBox(voiceArray, voiceNumber);
                     LoadPitchMappingInputTextBox(voiceArray, voiceNumber);
                     LoadScaleOptionsInputTextBox(voiceArray, voiceNumber);
@@ -105,23 +125,24 @@ $(document).ready(function()
             }
             else
             {
-                
                 $dnaLabel.hide();
                 $sequenceInput.hide();
+                
                 $letterA.hide();
                 $letterT.hide();
                 $letterU.hide();
                 $letterC.hide();
                 $letterG.hide();
+                
                 $letterAText.hide();
                 $letterUText.hide();
                 $letterTText.hide();
                 $letterCText.hide();
                 $letterGText.hide();
+                
                 $buttonConvert.hide();
                 $buttonDuplicate.hide();
             }
-			
         if($SelectedAlgorithm.text() == "Custom")
         {
             $TextBox.prop("readonly", false);
@@ -202,8 +223,8 @@ $(document).ready(function()
             UpdatePitchMappingArray(voiceArray[voiceNumber - 1], GetCurrentSelectedPitchMappingAlgorithm(voiceNumber), voiceArray[voiceNumber - 1].pitchMappingArrayLowerBound, voiceArray[voiceNumber - 1].pitchMappingArrayUpperBound);// this needs to be updating the other panels.
             UpdateFinalPitchArray(voiceArray[voiceNumber - 1], GetCurrentSelectedScale(voiceNumber), voiceArray[voiceNumber - 1].pitchMappingArrayLowerBound, voiceArray[voiceNumber - 1].pitchMappingArrayUpperBound);// this needs to be updating the other panels.
             
-            UpdateDurationNoteCount(voiceArray[voiceNumber - 1], voiceNumber);// these two are issues.
-            UpdateDurationMappingNoteCount(voiceArray[voiceNumber - 1]);// these two are issues.
+            UpdateDurationNoteCount(voiceArray[voiceNumber - 1], voiceNumber);
+            UpdateDurationMappingNoteCount(voiceArray[voiceNumber - 1]);
             
             $TextBox.val(voiceArray[voiceNumber - 1].originalPitchArray);
             UpdateDurationInputTextBoxFromPitchInput(voiceArray[voiceNumber - 1], voiceNumber);
@@ -222,11 +243,11 @@ $(document).ready(function()
         This is called when user types into a text box.
     */
     $('.pitch_input').on('change', '[id^=areaPitch]', function () {
-        var UpperNoteLimit = 2000;
+        var UpperNoteLimit = 2000; //  This is the max note count.
 	    var $panel = $(this).closest('div[id]');
 	    var $NoteCount = $panel.find('[id^=note_count]');
 	    var $TextBox = $panel.find('[id^=areaPitch]');
-	    var voiceNumber = getVoiceNumber($panel);
+	    var voiceNumber = getVoiceNumber($panel); // getVoiceNumber is in WebSiteFunctions.js
 
 	    var TextData = document.getElementById($TextBox.attr('id')).value;
 	    var candidateArray = TextData.split(",");
@@ -234,8 +255,10 @@ $(document).ready(function()
 	    if(candidateArray.length > UpperNoteLimit)
 	    {
 	        $NoteCount.val(UpperNoteLimit);
+	        
 	        var DifferenceInLength = candidateArray.length - UpperNoteLimit;
 	        candidateArray.length -= DifferenceInLength;
+            // If the length decreases, we use weird javascript technique of reducing the array length.
             if (ValidateCustomData(candidateArray) == false)
 	        {
 	            alert("Make Sure All Data Entered Is An Integer");
@@ -288,7 +311,7 @@ $(document).ready(function()
 	/*
 		Maintained from previous version.
 	*/
-
+	// this may be duplicated in WebSiteFunctions.js
 	function tooltip($parentId){
 		$infoTooltip = $parentId.find('[id^=pitchInfo]');
 		var $input = $parentId.find('[id^=input_set]');		
@@ -374,7 +397,6 @@ function populateDurationCustomTextInPitchInput(voices, newNoteCount, oldNoteCou
         voices.originalDurationArray.push(1);
     }
 }
-
 
 /*
 	Used to load the default input values for pitch
