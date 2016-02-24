3e653f254d46eaa4b191c867e1f584bf57870dc9
Evan Nilson <enilson@eagles.ewu.edu>
2016-02-24 01:26:42 -0800

    Finished utilities
    Added 'separate Char' feature. Utilities available in all sections
    except play. Changes in utility can only be applied in pitch/duration
    input.

--- js/scaleOptions.js
+++ js/scaleOptions.js
@@ -325,7 +325,9 @@ function scaleOptions(numberOfVoice) {
 					<option>A&#9839;/B&#9837;</option>\
 					<option>B</option>\
 				</select>\
-				<label>Output:</label><br />\
+				<label id='util"+ voiceCount+"'>  Utilities:</label>\
+				<button type='button' class='btn btn-default btn-sm' data-toggle='modal' onclick='openUtilities(\"scaleUtilityModal\",\"so_text_area\","+voiceCount+",\"scaleUtilityBox\")'><span class='glyphicon glyphicon-wrench'></span></button><br>\
+				<br /><label>Output:</label><br />\
 				<textarea readonly id='so_text_area"+ voiceCount + "'></textarea>\
 			</fieldset>\
 		</div>\
